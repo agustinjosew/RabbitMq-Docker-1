6ceac901d9e7331dd6030aac014ee5001cb1bdb0
Agustin j. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2022-06-07 15:21:09 -0300

    LogProductor implementation

--- LogProductor/Program.cs
+++ LogProductor/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace LogProductor
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // entry point to the rabbitMQ AMQP (Advanced Message Queuing Protocol) Client
+            ConnectionFactory factory = new ConnectionFactory();
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            Console.WriteLine("Please type your message.");
+            Console.WriteLine("Type 'exit' to exit.");
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+                if (message == "exit")
+                {
+                    connection.Close();
+                    break;
+                }
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine($"\t[x] Sent {message}");
+            }
+
         }
     }
 }
