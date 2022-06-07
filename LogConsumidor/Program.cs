0a7e0351553c1787386f9c069ae450564849fbd0
Agustin j. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2022-06-07 14:50:24 -0300

    Add project files.

---
+++ LogConsumidor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogConsumidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LogProductor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogProductor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
